d3f8c77f9a926ef5f41360a580cfda96a885e8ca
owner <dongyeon94@naver.com>
2020-07-01 17:34:48 +0900

    django fileupload setting

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http  import HttpResponse, Http404
 
 import subprocess
+import yangConverter
+import YangRegex
 import os
 
 def main(request):
@@ -18,12 +20,33 @@ def upload(request):
                     destination.write(chunk)
         process(x)
 
-        file_list = subprocess.check_output(['find /home/ec2-user/django/EC2Django/mysite/Files/data -mindepth 1'] ,shell=True ,encoding='utf-8')
-        file_list = file_list.split('\n')
+    file_list = subprocess.check_output(['find /home/ec2-user/django/EC2Django/mysite/Files/data -mindepth 1'] ,shell=True ,encoding='utf-8')
+    file_list = file_list.split('\n')
 
-        os.system('pyang -f jstree -o {}  {}'.format('/home/ec2-user/django/EC2Django/mysite/Files/result/out.html',
-                                                     ' '.join(file_list)))
+    total_Yang = ''
+    for file in file_list:
+        kk = ''
+        try:
+            with open(file ,'r') as ff:
+                kk += ff.readlines()
+            total_Yang += kk
+        except:
+            pass
+    with open('tmp.yang','w') as ft:
+        ft.write(total_Yang)
 
+
+
+    os.system('pyang -f jstree -o {}  {}'.format('/home/ec2-user/django/EC2Django/mysite/Files/result/out.html'
+                                           , ' '.join(file_list)))
+
+    A = YangRegex.regexClass('tmp.yang')
+    dic = A.leaf_dicOut()
+    B = yangConverter.Pharser('/home/ec2-user/django/EC2Django/mysite/Files/result/out.html')
+    out = B.finalFile(dic)
+
+    with open('{}.html'.format('ttest'), 'w') as fi:
+        fi.write(out)
 
     return render(request,"polls/uploadResult.html" ,{"data":file_list})
 
